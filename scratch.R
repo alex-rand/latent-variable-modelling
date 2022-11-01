ac7afcd29a3f21154bb1554785d9f8615e013b74
alex-rand <alexrand94@gmail.com>
2022-11-01 11:26:42 -0400

    working on it

--- scratch.R
+++ scratch.R
@@ -25,7 +25,7 @@ h1.summary <- summary(h1.fit, fit.measures = TRUE, standardized = TRUE)
 
 
   
-h1.summary$pe %>% 
+h1.summary$pe %>%  
   
   as_tibble() %>% 
  
@@ -47,38 +47,55 @@ h1.summary$pe %>%
     values_fill = " " 
   ) %>% 
   
-  column_to_rownames("rhs") %>% 
+  column_to_rownames("rhs") 
 
-  knitr::kable(
 
-  
-  select()
+# Define the relationships from my hypothesis
+h1_orthogonal.definition <- 
+  'map=~ags1+ags5+ags7
+mav=~ags2+ags6+ags12
+pap=~ags3+ags9+ags11
+pav=~ags4+ags8+ags10
 
-tibble(
-  Test             = "standard chi-squared",
-  `DF`             = h1.summary$test$standard$df,
-  `Test Statistic` = round(h1.summary$test$standard$stat, 2),
-  `p-value`        = h1.summary$test$standard$pvalue
-) %>% 
-  
-  mutate(across(everything(), as.character)) %>% 
-  
-  pivot_longer()
-  
-  
+map ~~ 1*mav
+pap ~~ 1*pav
+'
 
-factor_loadings <- h1.summary 
+# Fit the model
+h1_orthogonal.fit <- cfa(
+  data  = dat_ags,
+  model = h1_orthogonal.definition
+)
 
-h1.summary$pe %>% 
-  
-  slice(1:12) %>% 
-  
-  
-  
-  
-factor
+# Look at the results
+h1_orthogonal.summary <- summary(h1_orthogonal.fit, fit.measures = TRUE, standardized = TRUE)
 
-what$pe
-h1.cfa()
+h1_orthogonal.summary
 
-achievement.goal.cfa4.model
+h1_parsimonious.definition <- 
+'mastery=~ags1+ags5+ags7+ags2+ags6+ags12
+ performance=~ags3+ags9+ags11+ags4+ags8+ags10
+'
+
+# Fit the model
+h1_parsimonious.fit <- cfa(
+  data  = dat_ags,
+  model = h1_parsimonious.definition
+)
+
+# Look at the results
+h1_parsimonious.summary <- summary(h1_parsimonious.fit, fit.measures = TRUE, standardized = TRUE)
+h1_parsimonious.summary
+h1.summary
+
+
+anova(h1.fit, h1_orthogonal.fit) %>% 
+  
+  knitr::kable()
+anova(h1_parsimonious.fit, h1.fit)
+
+summary(h1_parsimonious.fit, fit.measures = TRUE)
+
+
+
+anova(h1.fit)
