b8d230657bd7bc20a478907ecaa33a8c8b332b3b
alex-rand <alexrand94@gmail.com>
2022-11-02 14:18:10 -0400

    more work

--- scratch.R
+++ scratch.R
@@ -20,58 +20,35 @@ h1.fit <- cfa(
   model = h1.definition
 )
 
-
 h1.summary <- summary(h1.fit, fit.measures = TRUE, standardized = TRUE)
 
 h1.summary
 
-  
-h1.summary$pe %>%  
-  
-  as_tibble() %>% 
- 
-  # Keep only the rows with info on factor loadings
-  slice(25:34) %>% 
- 
-  # Clean up the important values, then combine them into a single column
-
-  select(lhs, rhs, std.lv) %>% 
-  
-  mutate(
-    std.lv = round(std.lv, 2),
-    across(everything(), as.character)
-  ) %>% 
-  
-  pivot_wider(
-    names_from = "lhs", 
-    values_from = "std.lv",
-    values_fill = " " 
-  ) %>% 
-  
-  column_to_rownames("rhs") 
+semTools::AVE(h1.fit)
 
 
 # Define the relationships from my hypothesis
-h1_orthogonal.definition <- 
-  'map=~ags1+ags5+ags7
+h1_test.definition <- 
+'map =~ ags1 + a1*ags5 + a2*ags7
 mav=~ags2+ags6+ags12
 pap=~ags3+ags9+ags11
 pav=~ags4+ags8+ags10
 
-map ~~ 1*mav
-pap ~~ 1*pav
+a1 == a2
 '
 
 # Fit the model
-h1_orthogonal.fit <- cfa(
+h1_test.fit <- cfa(
   data  = dat_ags,
-  model = h1_orthogonal.definition
+  model = h1_test.definition
 )
 
 # Look at the results
-h1_orthogonal.summary <- summary(h1_orthogonal.fit, fit.measures = TRUE, standardized = TRUE)
+h1_test.summary <- summary(h1_test.fit, fit.measures = TRUE, standardized = TRUE)
 
-h1_orthogonal.summary
+h1_test.summary
+
+anova(h1_test.fit, h1.fit)
 
 h1_parsimonious.definition <- 
 'mastery=~ags1+ags5+ags7+ags2+ags6+ags12
